fb60df126883bd967532391504d4a45f9ecb3e6f
Estanislao Carrer <124751840+estani7@users.noreply.github.com>
2024-07-23 15:38:06 -0300

    Update ADC.c
    Corrección de un comentario

--- CDyM-TP04/ADC.c
+++ CDyM-TP04/ADC.c
@@ -1,29 +1,29 @@
 
 #include "ADC.h"
 
-uint8_t adc_flag;	   //se utiliza para avisar en el main que el módulo terminó la conversión
+uint8_t adc_flag;	   //se utiliza para avisar en el main que el mÃ³dulo terminÃ³ la conversiÃ³n
 volatile uint8_t data; //se utiliza para guardar los datos que arroja ADC
 
 void ADC_Init(void){
-	DDRC &=~(1<<DDC3);											// Se configura como entrada analógica el pin PC3
+	DDRC &=~(1<<DDC3);											// Se configura como entrada analÃ³gica el pin PC3
 	DIDR0 |= (1<<ADC3D);
 	
 	ADCSRB = 0x00;												// Se configura el FREE RUNNING mode
 	
 	ADCSRA |= (1 << ADEN) | (1 << ADPS1) | (1 << ADPS0);        // ADEN=1 Activa el ADC / Preescaler -> 8, entonces ADPS1=ADPS0=1 (revisar tabla)
 
-	ADMUX |= (1<<REFS0) | (1<<ADLAR) | (1<<MUX1) | (1<<MUX0);   //REFS1=0 & REFS0=1 -> modo ADCC en VREF / ADLAR=1 resultados a la izq (parte alta) / MUX1=MUX0=1 Se utiliza ADC3 como canal
+	ADMUX |= (1<<REFS0) | (1<<ADLAR) | (1<<MUX1) | (1<<MUX0);   //REFS1=0 & REFS0=1 -> modo AVCC en VREF / ADLAR=1 resultados a la izq (parte alta) / MUX1=MUX0=1 Se utiliza ADC3 como canal
 }
 
 void ADC_Read(void){
-	ADCSRA |= (1<<ADSC);				// Se inicia la conversión
-	while ((ADCSRA & (1<<ADIF))==0);	// Se espera el termino de la conversión
+	ADCSRA |= (1<<ADSC);				// Se inicia la conversiÃ³n
+	while ((ADCSRA & (1<<ADIF))==0);	// Se espera el termino de la conversiÃ³n
 	
 	ADCSRA |= (1<<ADIF);				// Se borra el flag
 	
 	data = ADCH;						// Guardo efectivamente los datos que arroja ADC (configurado para recibirlos en la parte alta)
 	
-	adc_flag = 1;						// Flag de término de conversión
+	adc_flag = 1;						// Flag de tÃ©rmino de conversiÃ³n
 }
 
 void adc_setFlag(uint8_t f){
